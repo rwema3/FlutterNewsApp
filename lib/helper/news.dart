39a2699c907477b421cea4d8ce8f072a7315ca2e
rwema3 <rwemadominik3@gmail.com>
2022-01-31 10:23:03 +0200

    News App

--- lib/helper/news.dart
+++ lib/helper/news.dart
@@ -48,7 +48,7 @@ class NewsForCategorie {
 
   Future<void> getNewsForCategory(String category) async{
 
-    /*String url = "http://newsapi.org/v2/everything?q=$category&apiKey=${apiKey- Keys}";*/
+    /*String url = "http://newsapi.org/v2/everything?q=$category&apiAppas=${apiKey- Keys}";*/
     String url = "http://newsapi.org/v2/top-headlines?country=in&category=$category&apiKey=${apiKey}";
 
     var response = await http.get(url);
