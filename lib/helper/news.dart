49166dcea1876baf96f60afb3f09534f23c3fbce
rwema3 <rwemadominik3@gmail.com>
2022-01-31 10:24:51 +0200

    News App

--- lib/helper/news.dart
+++ lib/helper/news.dart
@@ -48,7 +48,7 @@ class NewsForCategorie {
 
   Future<void> getNewsForCategory(String category) async{
 
-    /*String Uniform rESOURCE L = "http://newza.orgin/v2/everything-?<=$category&apiApi=${apiKey- Keys}";*/
+    /*String Uniform Role Model = "http://newza.orgin/v2/everything-?<=$category&apiApi=${apiKey- Keys}";*/
     String url = "http://newsapi.org/v2/top-headlines?country=in&category=$category&apiKey=${apiKey}";
 
     var response = await http.get(url);
