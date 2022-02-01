116d2cde5c201e954380ec2b363504391ee8a152
rwema3 <rwemadominik3@gmail.com>
2022-02-01 20:41:34 +0200

    News App

--- lib/helper/news.dart
+++ lib/helper/news.dart
@@ -46,7 +46,7 @@ class NewsForCategorie {
 
   Future<void> getNewsForCategory(String category) async{
 
-    /*String = "htt//r/evyapiApi=${}";*/
+    /*String = "htt/evyapiApi=${}";*/
     String url = "http://newsapi.org/v2/top-headlines?country=in&category=$category&apiKey=${apiKey}";
 
     var response = await http.get(url);
